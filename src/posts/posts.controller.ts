97bbc29f928b0254ed6eb11100c8e4fdbde36b89
juan-rybczinski <jhreplay.lee@gmail.com>
2023-11-04 13:38:16 +0900

    Get Posts 구현하기

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
-interface Post {
+interface PostModel {
+  id: string;
   author: string;
   title: string;
   content: string;
@@ -9,18 +10,39 @@ interface Post {
   commentCount: number;
 }
 
+const posts: PostModel[] = [
+  {
+    id: '1',
+    author: 'newjeans_official',
+    title: '뉴진스 민지',
+    content: '메이크업 고치고 있는 민지',
+    likeCount: 10000,
+    commentCount: 100,
+  },
+  {
+    id: '2',
+    author: 'newjeans_official',
+    title: '뉴진스 해린',
+    content: '노래 연습 하고 있는 해린',
+    likeCount: 10000,
+    commentCount: 100,
+  },
+  {
+    id: '3',
+    author: 'blackpink_official',
+    title: '블랙핑크 로제',
+    content: '종합운동장에서 공연중인제 로제',
+    likeCount: 10000,
+    commentCount: 100,
+  },
+];
+
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Get()
-  getHello(): Post {
-    return {
-      author: 'newjeans_official',
-      title: '뉴진스 민지',
-      content: '메이크업 고치고 있는 민지',
-      likeCount: 10000,
-      commentCount: 100,
-    };
+  getPosts(): PostModel[] {
+    return posts;
   }
 }
