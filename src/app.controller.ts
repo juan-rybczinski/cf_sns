e63bedf8fd707d5dac20ee707fd5adfaa810cdbb
juan-rybczinski <jhreplay.lee@gmail.com>
2023-11-04 12:17:56 +0900

    포스트 JSON 반환하기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): Post {
+    return {
+      author: 'newjeans_official',
+      title: '뉴진스 민지',
+      content: '메이크업 고치고 있는 민지',
+      likeCount: 10000,
+      commentCount: 100,
+    };
   }
 }
