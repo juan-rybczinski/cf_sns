cf3a2de83dc7bd93e2178bf280519e04536e8d23
juan-rybczinski <jhreplay.lee@gmail.com>
2023-11-07 00:06:36 +0900

    Delete 함수로 데이터 삭제하기

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -12,33 +12,6 @@ export interface PostModel {
   commentCount: number;
 }
 
-let posts: PostModel[] = [
-  {
-    id: 1,
-    author: 'newjeans_official',
-    title: '뉴진스 민지',
-    content: '메이크업 고치고 있는 민지',
-    likeCount: 10000,
-    commentCount: 100,
-  },
-  {
-    id: 2,
-    author: 'newjeans_official',
-    title: '뉴진스 해린',
-    content: '노래 연습 하고 있는 해린',
-    likeCount: 10000,
-    commentCount: 100,
-  },
-  {
-    id: 3,
-    author: 'blackpink_official',
-    title: '블랙핑크 로제',
-    content: '종합운동장에서 공연중인 로제',
-    likeCount: 10000,
-    commentCount: 100,
-  },
-];
-
 @Injectable()
 export class PostsService {
   constructor(
@@ -98,14 +71,16 @@ export class PostsService {
     return await this.postsRepository.save(post);
   }
 
-  deletePost(id: number) {
-    const post = posts.find((item) => item.id === +id);
+  async deletePost(id: number) {
+    const post = this.postsRepository.findOne({
+      where: { id },
+    });
 
     if (!post) {
       throw new NotFoundException();
     }
 
-    posts = posts.filter((post) => post.id !== +id);
+    await this.postsRepository.delete(id);
 
     return id;
   }
