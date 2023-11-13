dffaa622ab3534f839e1b2920d2750185864d96d
juan-rybczinski <jhreplay.lee@gmail.com>
2023-11-13 22:12:06 +0900

    composeFindOptions 함수 완성하기

--- src/common/common.service.ts
+++ src/common/common.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { BaseModel } from './entity/base.entity';
 import { BasePaginationDto } from './dto/base-pagination.dto';
 import {
@@ -7,6 +7,7 @@ import {
   FindOptionsWhere,
   Repository,
 } from 'typeorm';
+import { FILTER_MAPPER } from './const/filter-mapper.const';
 
 @Injectable()
 export class CommonService {
@@ -19,14 +20,14 @@ export class CommonService {
     path: string,
   ) {}
 
-  async cursorPaginate(
+  async cursorPaginate<T extends BaseModel>(
     dto: BasePaginationDto,
     repository: Repository<T>,
     overrideFindOptions: FindManyOptions<T> = {},
     path: string,
   ) {}
 
-  private async pagePaginate(
+  private async pagePaginate<T extends BaseModel>(
     dto: BasePaginationDto,
     repository: Repository<T>,
     overrideFindOptions: FindManyOptions<T> = {},
@@ -64,13 +65,45 @@ export class CommonService {
     key: string,
     value: any,
   ): FindOptionsWhere<T> {
-    return {};
+    const options: FindOptionsWhere<T> = {};
+
+    const split = key.split('__');
+    if (split.length !== 2 && split.length !== 3) {
+      throw new BadRequestException(
+        `where 필터는 '__'로 split 했을때 길이가 2 또는 3이어야합니다 - 문제되는 키값 : ${key}`,
+      );
+    }
+
+    if (split.length === 2) {
+      const [_, field] = split;
+      options[field] = value;
+    } else if (split.length === 3) {
+      const [_, field, operator] = split;
+      const values = value.split(',');
+      options[field] = FILTER_MAPPER[operator](
+        values.length > 1 ? values : value,
+      );
+    }
+
+    return options;
   }
 
   private parseOrderFilter<T extends BaseModel>(
     key: string,
     value: any,
   ): FindOptionsOrder<T> {
-    return {};
+    const options: FindOptionsOrder<T> = {};
+
+    const split = key.split('__');
+    if (split.length !== 2) {
+      throw new BadRequestException(
+        `where 필터는 '__'로 split 했을때 길이가 2이어야합니다 - 문제되는 키값 : ${key}`,
+      );
+    }
+
+    const [_, field] = split;
+    options[field] = value;
+
+    return options;
   }
 }
