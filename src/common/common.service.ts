f36f79c47d417a28268d291cf45a16a310419680
juan-rybczinski <jhreplay.lee@gmail.com>
2023-11-13 21:36:15 +0900

    작업할 Pagination 로직 정리하기

--- src/common/common.service.ts
+++ src/common/common.service.ts
@@ -5,10 +5,29 @@ import { FindManyOptions, Repository } from 'typeorm';
 
 @Injectable()
 export class CommonService {
+  prviate;
+
   paginate<T extends BaseModel>(
     dto: BasePaginationDto,
     repository: Repository<T>,
     overrideFindOptions: FindManyOptions<T> = {},
     path: string,
   ) {}
+
+  async cursorPaginate(
+    dto: BasePaginationDto,
+    repository: Repository<T>,
+    overrideFindOptions: FindManyOptions<T> = {},
+    path: string,
+  ) {}
+
+  private async pagePaginate(
+    dto: BasePaginationDto,
+    repository: Repository<T>,
+    overrideFindOptions: FindManyOptions<T> = {},
+  ) {}
+
+  private composeFindOptions<T extends BaseModel>(
+    dto: BasePaginationDto,
+  ): FindManyOptions<T> {}
 }
