b91ed8e7f944291489156f154025e9155f6f89d0
juan-rybczinski <jhreplay.lee@gmail.com>
2023-11-14 19:58:36 +0900

    Multer 세팅하기

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { BadRequestException, Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -6,6 +6,12 @@ import { PostsModel } from './entities/posts.entity';
 import { AuthModule } from '../auth/auth.module';
 import { UsersModule } from '../users/users.module';
 import { CommonModule } from '../common/common.module';
+import { MulterModule } from '@nestjs/platform-express';
+import { extname } from 'path';
+import * as multer from 'multer';
+import { POST_IMAGE_PATH } from '../common/const/paths.const';
+import e from 'express';
+import { v4 as uuid } from 'uuid';
 
 @Module({
   imports: [
@@ -13,6 +19,47 @@ import { CommonModule } from '../common/common.module';
     AuthModule,
     UsersModule,
     CommonModule,
+    MulterModule.register({
+      limits: {
+        fileSize: 10 * 1024 * 1024,
+      },
+      fileFilter(
+        req: any,
+        file: {
+          fieldname: string;
+          originalname: string;
+          encoding: string;
+          mimetype: string;
+          size: number;
+          destination: string;
+          filename: string;
+          path: string;
+          buffer: Buffer;
+        },
+        callback: (error: Error | null, acceptFile: boolean) => void,
+      ) {
+        const ext = extname(file.originalname);
+        if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png') {
+          return callback(
+            new BadRequestException('jpg/jpeg/png 파일만 업로드 가능합니다!'),
+            false,
+          );
+        }
+        return callback(null, true);
+      },
+      storage: multer.diskStorage({
+        destination: (req, res, cb) => {
+          cb(null, POST_IMAGE_PATH);
+        },
+        filename(
+          req: e.Request,
+          file: Express.Multer.File,
+          callback: (error: Error | null, filename: string) => void,
+        ) {
+          callback(null, `${uuid()}${extname(file.originalname)}`);
+        },
+      }),
+    }),
   ],
   controllers: [PostsController],
   providers: [PostsService],
