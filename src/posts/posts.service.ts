9efc2cf9d875d84f6f55cc158a60f64064871c7e
juan-rybczinski <jhreplay.lee@gmail.com>
2023-11-14 20:58:39 +0900

    엔티티가 생성될때 임시 폴더로부터 이미지 파일 이동시키기

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { PostsModel } from './entities/posts.entity';
@@ -6,6 +10,9 @@ import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { PaginatePostDto } from './dto/paginate-post.dto';
 import { CommonService } from '../common/common.service';
+import { basename, join } from 'path';
+import { POST_IMAGE_PATH, TEMP_FOLDER_PATH } from '../common/const/paths.const';
+import { promises } from 'fs';
 
 @Injectable()
 export class PostsService {
@@ -56,7 +63,24 @@ export class PostsService {
     }
   }
 
+  async createPostImage(dto: CreatePostDto) {
+    const tempFilePath = join(TEMP_FOLDER_PATH, dto.image);
+    try {
+      await promises.access(tempFilePath);
+    } catch (e) {
+      throw new BadRequestException('임시 파일이 존재하지 않습니다!');
+    }
+
+    const fileName = basename(tempFilePath);
+    const publicFilePath = join(POST_IMAGE_PATH, fileName);
+    await promises.rename(tempFilePath, publicFilePath);
+
+    return true;
+  }
+
   async createPost(authorId: number, postDto: CreatePostDto) {
+    await this.createPostImage(postDto);
+
     const post = this.postsRepository.create({
       author: {
         id: authorId,
