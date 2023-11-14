48698821e0a8efe0750c7f157dc3a488141d87db
juan-rybczinski <jhreplay.lee@gmail.com>
2023-11-14 20:34:15 +0900

    이미지 업로드 엔드포인트 생성하기

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,8 +1,61 @@
-import { Module } from '@nestjs/common';
+import { BadRequestException, Module } from '@nestjs/common';
 import { CommonService } from './common.service';
 import { CommonController } from './common.controller';
+import { MulterModule } from '@nestjs/platform-express';
+import { extname } from 'path';
+import * as multer from 'multer';
+import { TEMP_FOLDER_PATH } from './const/paths.const';
+import e from 'express';
+import { v4 as uuid } from 'uuid';
+import { AuthModule } from '../auth/auth.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
+  imports: [
+    MulterModule.register({
+      limits: {
+        fileSize: 10 * 1024 * 1024,
+      },
+      fileFilter(
+        req: any,
+        file: {
+          fieldname: string;
+          originalname: string;
+          encoding: string;
+          mimetype: string;
+          size: number;
+          destination: string;
+          filename: string;
+          path: string;
+          buffer: Buffer;
+        },
+        callback: (error: Error | null, acceptFile: boolean) => void,
+      ) {
+        const ext = extname(file.originalname);
+        if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png') {
+          return callback(
+            new BadRequestException('jpg/jpeg/png 파일만 업로드 가능합니다!'),
+            false,
+          );
+        }
+        return callback(null, true);
+      },
+      storage: multer.diskStorage({
+        destination: (req, res, cb) => {
+          cb(null, TEMP_FOLDER_PATH);
+        },
+        filename(
+          req: e.Request,
+          file: Express.Multer.File,
+          callback: (error: Error | null, filename: string) => void,
+        ) {
+          callback(null, `${uuid()}${extname(file.originalname)}`);
+        },
+      }),
+    }),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [CommonController],
   providers: [CommonService],
   exports: [CommonService],
