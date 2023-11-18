be67474501e6893c7f2554fbb241d6b2d7aae387
juan-rybczinski <jhreplay.lee@gmail.com>
2023-11-18 12:27:08 +0900

    서버에서 이벤트 전송하기

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -3,19 +3,23 @@ import {
   OnGatewayConnection,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
   namespace: 'chats',
 })
 export class ChatsGateway implements OnGatewayConnection {
+  @WebSocketServer()
+  server: Server;
+
   handleConnection(socket: Socket): any {
     console.log(`On connect called... ${socket.id}`);
   }
 
   @SubscribeMessage('send_message')
   sendMessage(@MessageBody() message: string) {
-    console.log(message);
+    this.server.emit('receive_message', 'Hello from server!');
   }
 }
