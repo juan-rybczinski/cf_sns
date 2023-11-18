921f5973e121e2e2a7daef8e47dc4ca0346f05ed
juan-rybczinski <jhreplay.lee@gmail.com>
2023-11-18 16:14:43 +0900

    Chat Entity 생성하기

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -7,6 +7,8 @@ import {
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { ChatsService } from './chats.service';
+import { CreateChatDto } from './dto/create-chat.dto';
 
 @WebSocketGateway({
   namespace: 'chats',
@@ -15,8 +17,15 @@ export class ChatsGateway implements OnGatewayConnection {
   @WebSocketServer()
   server: Server;
 
+  constructor(private readonly chatService: ChatsService) {}
+
   handleConnection(socket: Socket): any {
     console.log(`On connect called... ${socket.id}`);
+  }
+
+  @SubscribeMessage('create_chat')
+  async createChat(@MessageBody() data: CreateChatDto) {
+    const chat = await this.chatService.createChat(data);
   }
 
   @SubscribeMessage('enter_chat')
--- src/chats/chats.service.ts
+++ src/chats/chats.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ChatsModel } from './entity/chats.entity';
+import { Repository } from 'typeorm';
+import { CreateChatDto } from './dto/create-chat.dto';
 
 @Injectable()
-export class ChatsService {}
+export class ChatsService {
+  constructor(
+    @InjectRepository(ChatsModel)
+    private readonly chatsRepository: Repository<ChatsModel>,
+  ) {}
+
+  async createChat(dto: CreateChatDto) {
+    const chat = await this.chatsRepository.save({
+      users: dto.userIds.map((id) => ({
+        id,
+      })),
+    });
+
+    return this.chatsRepository.findOne({
+      where: {
+        id: chat.id,
+      },
+    });
+  }
+}
